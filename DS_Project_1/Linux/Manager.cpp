af699fd5c32a1d5661a96b1f5b520a3cd459e342
YEONGORI <dusrjf14@gmail.com>
2022-09-30 16:35:28 +0900

    q

--- DS_Project_1/Linux/Manager.cpp
+++ DS_Project_1/Linux/Manager.cpp
@@ -21,29 +21,21 @@ void Manager::Run(const char* filepath)
     fdata.open("filesnumbers.csv");
 
     char cmd[32];
-
     while (!fin.eof())
     {
         //Read the command
         fin.getline(cmd, 32);
         char* tmp = strtok(cmd, " ");
-
         if (strcmp(tmp, "LOAD") == 0)
         {
             fout << "=========LOAD=========" << endl;
-
             while (!fdata.eof())
             {
-                int index;
-                string raw;
-                fdata >> index;
-                fdata >> raw;
-                cout << index << raw << endl;
+                char raw1[100], raw2[100];
+                fdata.getline(raw1, sizeof(raw1), ',');
+                fdata.getline(raw2, sizeof(raw2) , '\n');
             }
-
-
         }
-
     }
 
     // TODO: implement
